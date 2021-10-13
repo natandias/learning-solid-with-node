0b6273c0739f18a70bc4d89b39ab307cf52f4eaa
natandias <natandias@pm.me>
2021-10-12 21:41:48 -0300

    Feat: remove unused config file

--- src/database/config.ts
+++
@@ -1,6 +0,0 @@
-import { createConnection } from 'typeorm';
-import { SqliteTypeOrmConnection } from './connections';
-
-const connection = createConnection(SqliteTypeOrmConnection);
-
-export default connection;
